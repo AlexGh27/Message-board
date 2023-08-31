a964b1a973757f1b497f93085564352556e379bd
AlexGh27 <alexghioca27@gmail.com>
2023-08-31 13:40:46 +0300

    Initial push

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {})
+});
+
+router.get('/', (req, res) => {
+  
+  const newMessage = {
+      text: req.body.message,
+      user: req.body.author,
+      added: new Date()
+  };
+
+  messages.push(newMessage);
+
+  res.redirect('/');
+});
+
+module.exports = router;
